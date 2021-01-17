eacdcb3877d9b6efc17ade732ff3861a278a7b02
R4HANDON <44488041+rondinelle03@users.noreply.github.com>
2021-01-16 23:10:06 -0300

    Parte uma finalizada!
    Inicialização do esqueleto

---
+++ libs/geral.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+
+var nav = $('.topo');
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+    });
+
+});
