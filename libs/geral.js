c3375e63a9e4f02a76b390bc4a81de733c62b8f0
R4HANDON <44488041+rondinelle03@users.noreply.github.com>
2021-01-25 22:41:42 -0300

    Modais de Sobre e Ponto Finalizado
    páginas finalizadas.

--- libs/geral.js
+++ libs/geral.js
@@ -21,10 +21,33 @@ $(document).ready(function(){
 
 });
 
+/* ALIMENTAÇÃO */
+
 function openNav() {
   document.getElementById("mySidenav").style.height = "100%";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.height = "0";
+}
+
+/* SOBRE */
+
+function openSobre() {
+  document.getElementById("mySidenavSobre").style.height = "100%";
+}
+
+function closeSobre() {
+  document.getElementById("mySidenavSobre").style.height = "0";
+}
+
+
+/* PONTO DIGITAL */
+
+function openPontodigital() {
+  document.getElementById("mySidenavPonto").style.height = "100%";
+}
+
+function closePontodigital() {
+  document.getElementById("mySidenavPonto").style.height = "0";
 }
