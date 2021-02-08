ce0ceb5e2ec7d42a08cae2737ca49910bcbc52e8
R4HANDON <44488041+rondinelle03@users.noreply.github.com>
2021-02-08 18:50:47 -0300

    Listagem com tabelas
    Listagem com tabelas finalizadas

--- libs/geral.js
+++ libs/geral.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+   $('#example').DataTable( {
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }
+    } );
+ 
   /*$(".mn_sub").click(function(){
     $("a").toggleClass("ativado");
   });*/
